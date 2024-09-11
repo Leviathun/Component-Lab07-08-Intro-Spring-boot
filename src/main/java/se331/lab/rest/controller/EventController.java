2aa468bd4a31657780ef387be17346eb0a2900da
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-09-11 23:28:55 +0700

    2.5-2.8 Add controller

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -1,4 +1,94 @@
 package se331.lab.rest.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import se331.lab.rest.entity.Event;
+
+import jakarta.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class EventController {
+    List<Event> eventList;
+
+    @GetMapping("events")
+    public ResponseEntity<?> getEventLists() {
+        return ResponseEntity.ok(eventList);
+    }
+
+    @PostConstruct
+    public void init () {
+        eventList = new ArrayList<>();
+        eventList.add(Event.builder()
+                .id(123L)
+                .category("animal welfare")
+                .title("Cat Adoption Day")
+                .description("Find your new feline friend at this event.")
+                .location("Meow Town")
+                .date("January 28, 2022")
+                .time("12:00")
+                .petAllowed(true)
+                .organizer("Kat Laydee")
+                .build());
+        eventList.add(Event.builder()
+                .id(456L)
+                .category("food")
+                .title("Community Gardening")
+                .description("Join us as we tend to the community edible plants.")
+                .location("Flora City")
+                .date("March 14, 2022")
+                .time("10:00")
+                .petAllowed(true)
+                .organizer("Fern Pollin")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(789L)
+                .category("sustainability")
+                .title("Beach Cleanup")
+                .description("Help pick up trash along the shore.")
+                .location("Playa Del Carmen")
+                .date("July 22, 2022")
+                .time("11:00")
+                .petAllowed(false)
+                .organizer("Carey Wales")
+                .build());
+        eventList.add(Event.builder()
+                .id(1001L)
+                .category("animal welfare")
+                .title("Dog Adoption Day")
+                .description("Find your new canine friend at this event.")
+                .location("Woof Town")
+                .date("August 28, 2022")
+                .time("12:00")
+                .petAllowed(true)
+                .organizer("Dawg Dahd")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(1002L)
+                .category("food")
+                .title("Canned Food Drive")
+                .description("Bring your canned food to donate to those in need.")
+                .location("Tin City")
+                .date("September 14, 2022")
+                .time("3:00")
+                .petAllowed(true)
+                .organizer("Kahn Opiner")
+                .build());
+        eventList.add(Event.builder()
+                .id(1003L)
+                .category("sustainability")
+                .title("Highway Cleanup")
+                .description("Help pick up trash along the highway.")
+                .location("Highway 50")
+                .date("July 22, 2022")
+                .time("11:00")
+                .petAllowed(false)
+                .organizer("Brody Kill")
+                .build());
+    }
 }
+
