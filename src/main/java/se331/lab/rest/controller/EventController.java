5f35867940fa7844dc795e9b823b290f26e7af96
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-09-12 22:52:10 +0700

    3.3 Add path variable query

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -1,9 +1,12 @@
 package se331.lab.rest.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 import se331.lab.rest.entity.Event;
 
 import jakarta.annotation.PostConstruct;
@@ -28,6 +31,22 @@ public class EventController {
             return ResponseEntity.ok(output);
         }catch (IndexOutOfBoundsException ex) {
             return ResponseEntity.ok(output);
+        }
+    }
+
+    @GetMapping("events/{id}")
+    public ResponseEntity<?> getEvent(@PathVariable("id") Long id) {
+        Event output = null;
+        for (Event event : eventList) {
+            if (event.getId().equals(id) ) {
+                output = event;
+                break;
+            }
+        }
+        if (output != null) {
+            return ResponseEntity.ok(output);
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
         }
     }
 
