27f6b4c6859d9123d9d733168b469e8424eb976c
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-09-12 23:18:57 +0700

    4.3 CORS

--- src/main/java/se331/lab/rest/Application.java
+++ src/main/java/se331/lab/rest/Application.java
@@ -2,6 +2,9 @@ package se331.lab.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,14 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer () {
+		return new WebMvcConfigurer () {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping ("/events")
+						.allowedOrigins ("http://localhost:5173");
+			}
+		};
+	}
 }
