4fb28f6c55af06cd1cbea5f2446c883e689ec4bf
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-09-13 10:04:09 +0700

    5.4 Update EventController

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -1,5 +1,6 @@
 package se331.lab.rest.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,26 +12,24 @@ import org.springframework.web.server.ResponseStatusException;
 import se331.lab.rest.entity.Event;
 
 import jakarta.annotation.PostConstruct;
+import se331.lab.rest.service.EventService;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class EventController {
-    List<Event> eventList;
+    final EventService eventService;
 
     @GetMapping("events")
     public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit",required = false)Integer perPage, @RequestParam(value = "_page",required = false)Integer page) {
-        perPage = perPage == null?eventList.size():perPage;
-        page = page == null?1:page;
-        Integer firstIndex = (page-1)*perPage;
-
-        List<Event> output = new ArrayList<>();
+        List<Event> output = null;
+        Integer eventSize = eventService.getEventSize();
         HttpHeaders responseHeader = new HttpHeaders();
-        responseHeader.set("x-total-count", String.valueOf(eventList.size()));
+        responseHeader.set("x-total-count", String.valueOf(eventSize));
         try {
-            for (int i = firstIndex; i < firstIndex + perPage; i++) {
-                output.add(eventList.get(i));
-            }
+            output = eventService.getEvents(perPage, page);
             return new ResponseEntity<>(output, responseHeader, HttpStatus.OK) ;
         }catch (IndexOutOfBoundsException ex) {
             return new ResponseEntity<>(output, responseHeader, HttpStatus.OK) ;
@@ -39,91 +38,12 @@ public class EventController {
 
     @GetMapping("events/{id}")
     public ResponseEntity<?> getEvent(@PathVariable("id") Long id) {
-        Event output = null;
-        for (Event event : eventList) {
-            if (event.getId().equals(id) ) {
-                output = event;
-                break;
-            }
-        }
+        Event output = eventService.getEvent(id);
         if (output != null) {
             return ResponseEntity.ok(output);
         }else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
         }
-    }
-
-    @PostConstruct
-    public void init () {
-        eventList = new ArrayList<>();
-        eventList.add(Event.builder()
-                .id(123L)
-                .category("animal welfare")
-                .title("Cat Adoption Day")
-                .description("Find your new feline friend at this event.")
-                .location("Meow Town")
-                .date("January 28, 2022")
-                .time("12:00")
-                .petAllowed(true)
-                .organizer("Kat Laydee")
-                .build());
-        eventList.add(Event.builder()
-                .id(456L)
-                .category("food")
-                .title("Community Gardening")
-                .description("Join us as we tend to the community edible plants.")
-                .location("Flora City")
-                .date("March 14, 2022")
-                .time("10:00")
-                .petAllowed(true)
-                .organizer("Fern Pollin")
-                .build());
-
-        eventList.add(Event.builder()
-                .id(789L)
-                .category("sustainability")
-                .title("Beach Cleanup")
-                .description("Help pick up trash along the shore.")
-                .location("Playa Del Carmen")
-                .date("July 22, 2022")
-                .time("11:00")
-                .petAllowed(false)
-                .organizer("Carey Wales")
-                .build());
-        eventList.add(Event.builder()
-                .id(1001L)
-                .category("animal welfare")
-                .title("Dog Adoption Day")
-                .description("Find your new canine friend at this event.")
-                .location("Woof Town")
-                .date("August 28, 2022")
-                .time("12:00")
-                .petAllowed(true)
-                .organizer("Dawg Dahd")
-                .build());
-
-        eventList.add(Event.builder()
-                .id(1002L)
-                .category("food")
-                .title("Canned Food Drive")
-                .description("Bring your canned food to donate to those in need.")
-                .location("Tin City")
-                .date("September 14, 2022")
-                .time("3:00")
-                .petAllowed(true)
-                .organizer("Kahn Opiner")
-                .build());
-        eventList.add(Event.builder()
-                .id(1003L)
-                .category("sustainability")
-                .title("Highway Cleanup")
-                .description("Help pick up trash along the highway.")
-                .location("Highway 50")
-                .date("July 22, 2022")
-                .time("11:00")
-                .petAllowed(false)
-                .organizer("Brody Kill")
-                .build());
     }
 }
 
