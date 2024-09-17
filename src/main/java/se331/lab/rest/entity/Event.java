eb5baa6e83eb67265d0c6c37252188c467bcadf0
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-09-17 20:46:28 +0700

    2.1-2.2 Add Spring boot JPA & update Event class

--- src/main/java/se331/lab/rest/entity/Event.java
+++ src/main/java/se331/lab/rest/entity/Event.java
@@ -1,11 +1,21 @@
 package se331.lab.rest.entity;
 
-import lombok.Builder;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Data
 @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
     Long id;
     String category;
     String title;
