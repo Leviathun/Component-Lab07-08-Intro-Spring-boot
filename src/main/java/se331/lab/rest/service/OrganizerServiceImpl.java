90775621292983c50c3720fc70d8517116a61793
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-10-13 20:32:54 +0700

    fix bug with OrganizerServiceImpl

--- src/main/java/se331/lab/rest/service/OrganizerServiceImpl.java
+++ src/main/java/se331/lab/rest/service/OrganizerServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import se331.lab.rest.controller.OrganizerDao;
+import se331.lab.rest.dao.OrganizerDao;
 import se331.lab.rest.entity.Organizer;
 
 import java.util.List;
